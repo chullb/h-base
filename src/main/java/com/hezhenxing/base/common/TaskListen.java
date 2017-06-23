f83f2327905f193024d6351de7e88787c418e3bc
何振兴 <chullb@163.com>
2017-06-23 10:01:55 +0800

    【系统优化】 去掉部分文件

--- src/main/java/com/hezhenxing/base/common/TaskListen.java
+++ src/main/java/com/hezhenxing/base/common/TaskListen.java
@@ -1,7 +1,5 @@
 package com.hezhenxing.base.common;
 
-import com.hezhenxing.base.common.Thread.TestThread;
-import com.hezhenxing.base.common.Thread.ThreadHandle;
 import org.apache.log4j.Logger;
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.context.support.WebApplicationContextUtils;
@@ -24,14 +22,10 @@ public class TaskListen implements ServletContextListener {
         ServletContext servletcontext = servletContextEvent.getServletContext();
         WebApplicationContext applicationContext = WebApplicationContextUtils.getWebApplicationContext(servletcontext);
         SpringContextUtil.setApplicationContextStaticlly(applicationContext);
-        //测试线程
-        for (int i = 0; i < 5; i++) {
-            ThreadHandle.MarkingPool.execute(new TestThread());
-        }
+
     }
 
     public void contextDestroyed(ServletContextEvent servletContextEvent) {
         logger.info("程序结束执行TaskListen.contextDestroyed()..");
-        ThreadHandle.MarkingPool.shutdown();
     }
 }
